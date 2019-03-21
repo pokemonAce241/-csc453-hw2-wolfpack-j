294b19cbb7eb04fda4ac2cd6160bf34fd55f06ab
jdbencke <jdbencke@ncsu.edu>
2019-03-21 04:32:28 -0400

    Update Laptop2.py

--- Laptop2.py
+++ Laptop2.py
@@ -26,21 +26,30 @@ int message_size_mask = int("00011111",2)
 def main():
         connect()
         loop()
-#
+#This is the main loop of the program. This is resposible for recieving the data from the arduino and producing the proper output
+#based on parameters provided by the meassage header sent with the data
 def loop():
 while true:
         message_id = none
         while (Data.availabe() > 0):
             byte = Data.read() 
+#This variable extracts the message id that is 8 bits. This determines wiether or not the loop continues to extract data. Once all
+#data is sent the message_id should be a recieved message which is send to the socket after the while loop is over.
             message_id = byte
-#The following functions extract the message, output type,message size, and end of file information that is needed.
+#The following procedures extract the message, output type,message size, and end of file information that is needed.
+
+#The specification extracts the next part of the message header after the messageid.
             specification = Data.read()
             spec = int(specification,2)
                 
+#This is a 1 bit output type that informs the program weither to print the output or write it to a file.                
             result_type = spec&type_mask
             result_type = bin(result_type)
             result_type = result_type>>7
 
+#This is a 1 bit that describes weither this message is the end of a file. It is important for when the output a file since this
+#tells the program that this is the end of the file so that the program can properly close the current file and open up a new
+#file in case the user wants to continue sending output to a file.
             end_file = spec&file_end_mask
             end_file = bin(end_file)
             end_file = end_file<<1
