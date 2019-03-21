370d3fc46f9f1697f75e06138433ab14103e7260
jdbencke <jdbencke@ncsu.edu>
2019-03-20 21:32:43 -0400

    Update Laptop2.py

--- Laptop2.py
+++ Laptop2.py
@@ -3,8 +3,10 @@ import socket
 import time
 
 Data = serial.Serial('/dev/ttyACM0',9600)   
+
 host =
-port = 2424
+port = 12345
+send_socket = None
 
 
 
@@ -16,4 +18,16 @@ while (Data.availabe() > 0):
     Data.print(byte)
     
 print(data)
+
+
+def connect():
+    global send_socket, HOST, PORT
+    # establish a connection to the remote server
+    send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    send_socket.connect((HOST, PORT))
+        
+# Closes the send_socket
+def clean_up():
+    if (send_socket):
+        send_socket.close()      
 
